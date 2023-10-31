79586c20ae0723f8a189c507b943bb6e68bf2ea0
Theresia Artha Putri <therenoona@gmail.com>
2023-10-31 22:00:46 +0700

    Develop completing and adding task feature

--- resources/views/partials/task_card.blade.php
+++ resources/views/partials/task_card.blade.php
@@ -16,7 +16,7 @@
   </div>
   <div class="@if ($leftStatus) task-progress-card-left @else task-progress-card-right @endif">
     @if ($leftStatus)
-      // Mengapit "button" dengan "form"
+      
       <form
         action="{{ route('tasks.move', ['id' => $task->id, 'status' => $leftStatus]) }}" 
         method="POST"
@@ -28,7 +28,7 @@
     @endif
 
     @if ($rightStatus)
-      // Mengapit "button" dengan "form"
+      
       <form
         action="{{ route('tasks.move', ['id' => $task->id, 'status' => $rightStatus]) }}"
         method="POST"
